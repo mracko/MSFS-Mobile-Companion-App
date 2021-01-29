b706025ae1ac14d5096b93b01a27836e13eff40c
hankhank10 <m.a.hankinson@gmail.com>
2021-01-29 14:12:58 +0000

    Trying with debug

--- glass_server.py
+++ glass_server.py
@@ -140,7 +140,7 @@ def flask_thread_func(threadname):
 
     # END: Find my plane routes
 
-    app.run(host='0.0.0.0', port=4000, debug=False)
+    app.run(host='0.0.0.0', port=4000, debug=True, use_reloader=False)
 
 # SimConnect  App
 def simconnect_thread_func(threadname):
