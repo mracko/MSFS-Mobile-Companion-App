1977f687b6a96c8d811cb971b8ad345659629cee
hankhank10 <m.a.hankinson@gmail.com>
2021-01-29 15:14:22 +0000

    GUI update

--- static/js/custom/findMyPlane.js
+++ static/js/custom/findMyPlane.js
@@ -1,9 +1,48 @@
+let findmyplaneConnectionStatus;
+let findmyplaneIdentPublicKey;
+let findmyplaneLinkURL;
 
+findmyplaneConnectionStatus = "disconnected";
 
-function startFindmyplaneTracking() {
+function toggleFindmyplaneTracking() {
+
+    if (findmyplaneConnectionStatus === "disconnected") {
+        startFindmyplaneTracking()
+    }
+
+    if (findmyplaneConnectionStatus === "connected") {
+        stopFindmyplaneTracking()
+    }
 
 }
 
+function startFindmyplaneTracking() {
+    $.getJSON($SCRIPT_ROOT + '/findmyplane/status/set/connected', {}, function(data) {
+        if (data.status === 'connected') {
+            findmyplaneConnectionStatus = 'connected';
+            findmyplaneIdentPublicKey = data.ident_public_key;
+            findmyplaneLinkURL = data.url_to_view;
+
+            checkAndUpdateButton("#findmyplaneMaster", 1, "Connected", "Disconnected")
+
+            //$("findmyplaneConnectionStatus").html = "Connected"
+
+            window.alert("Connected")
+        }
+
+        if (data.status === 'error') {
+            findmyplaneConnectionStatus = 'disconnected'
+
+            checkAndUpdateButton("#findmyplaneMaster", 0)
+            window.alert("Errored")
+        }
+    });
+}
+
+
+
 function stopFindmyplaneTracking() {
 
+    window.alert("Pass")
+    
 }
